902430f605ec0d54e94337ec8c0b4e830611e1bf
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-07 17:44:28 +0000

    task7

---
+++ Javascript_Projects/task7/script.js
@@ -0,0 +1,13 @@
+let count = 0;
+let prg = document.querySelector("#progress");
+let h3 = document.querySelector("h3");
+
+
+let int = setInterval(function() {
+    if (count === 100) {
+        h3.style.opacity = 1;
+        clearInterval(int);
+    }
+    count++;
+    prg.style.width = count + '%';
+}, 100);
