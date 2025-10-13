c70dac68cdd49a956d3e3e3fbe97cc7621b8f19b
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-13 09:10:31 +0000

    gsap

---
+++ GSAP_Learning/Day2/script.js
@@ -0,0 +1,26 @@
+let t1 = gsap.timeline()
+
+t1.to(".box1", {
+    x: 1200,
+    rotate: 360,
+    scale: 0.5,
+    duration: 2,
+    delay: 1
+
+})
+t1.to(".box2", {
+    x: 1200,
+    rotate: 360,
+    scale: 0.5,
+    duration: 2
+        /*delay: 3*/
+
+})
+t1.to(".box3", {
+    x: 1200,
+    rotate: 360,
+    scale: 0.5,
+    duration: 2
+        /*delay: 6*/
+
+})
