c911db5e3b176d3b15de2796ac55f876613bd0c2
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-14 17:47:53 +0000

    scrolltrigger

---
+++ GSAP_Learning/Day4_ScrollTrigger/script.js
@@ -0,0 +1,28 @@
+gsap.from(".page1 .circle", {
+    scale: 0,
+    delay: 1,
+    duration: 2,
+    rotate: 360
+})
+gsap.from(".page2 .circle", {
+    scale: 0,
+    delay: 1,
+    duration: 2,
+    rotate: 360,
+    scrollTrigger: ".page2 .circle"
+
+})
+gsap.from(".page3 .circle", {
+    scale: 0,
+    delay: 1,
+    duration: 2,
+    rotate: 720,
+    scrollTrigger: {
+        trigger: ".page3 .circle",
+        scroller: "body",
+        markers: true,
+        start: "top 60%",
+        end: "top 30%",
+        scrub: 2
+    }
+})
