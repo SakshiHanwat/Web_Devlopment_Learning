28e86919eefc2adba559c56d36f0e568ef05c284
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-31 21:02:32 +0530

    Add files via upload

---
+++ ReactJs/First/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar> </Navbar> <h3> Hello World! </h3>{" "}
+    </div>
+  );
+}
+export default App;
